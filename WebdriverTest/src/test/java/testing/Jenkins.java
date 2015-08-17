6d8fd15bc290d0ef5abf740ecb6b59b3c3b83499
immusk <imranrehman5155@gmail.com>
2015-08-17 22:46:50 +0530

    this is my first commit

---
+++ WebdriverTest/src/test/java/testing/Jenkins.java
@@ -0,0 +1,21 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Jenkins {
+
+	public WebDriver driver;
+	@Test
+	public void verifyLogin(){
+		
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.co.in/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=birthday%20wishes");
+		
+		String title = "birthday wishes - Google Search";
+		Assert.assertEquals(driver.getTitle(),title, "Expected page is not opened");
+		driver.close();
+	}
+}
