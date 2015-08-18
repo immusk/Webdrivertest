e454a7c48517070e441c19a976baa9eda1a993f6
immusk <imranrehman5155@gmail.com>
2015-08-18 18:32:11 +0530

    added few more tests

--- WebdriverTest/src/test/java/testing/Jenkins.java
+++ WebdriverTest/src/test/java/testing/Jenkins.java
@@ -1,5 +1,6 @@
 package testing;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -8,14 +9,33 @@ import org.testng.annotations.Test;
 public class Jenkins {
 
 	public WebDriver driver;
+
 	@Test
-	public void verifyLogin(){
-		
+	public void openGoogle() {
+
 		driver = new FirefoxDriver();
-		driver.get("https://www.google.co.in/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=birthday%20wishes");
-		
-		String title = "birthday wishes - Google Search";
-		Assert.assertEquals(driver.getTitle(),title, "Expected page is not opened");
-		driver.close();
+		driver.get("https://www.google.co.in/");
+		Assert.assertEquals(driver.getTitle(), "Google", "Expected page is not opened");
+		driver.quit();
+	}
+	
+	@Test
+	public void doGoogleSearch() {
+
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		Assert.assertEquals(driver.getTitle(), "Google", "Expected page is not opened");
+		driver.quit();
+	}
+	
+	@Test
+	public void doYahooSearch() {
+
+		driver = new FirefoxDriver();
+		driver.get("https://www.yahoo.com/");
+		driver.findElement(By.id("UHSearchBox")).sendKeys("Selenium");
+		Assert.assertEquals(driver.getTitle(), "Yahoo", "Expected page is not opened");
+		driver.quit();
 	}
 }
